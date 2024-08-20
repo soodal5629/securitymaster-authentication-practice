501c192b71664879ab96a9aeec59f248f187f91b
jcl <chaelynjang@lotte.net>
2024-08-20 16:56:01 +0900

    커스텀하게 로그아웃 처리+타임리프 인증 여부에 따라 요소 가시 처리

--- src/main/java/io/security/authentication/users/controller/LoginController.java
+++ src/main/java/io/security/authentication/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package io.security.authentication.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,5 +17,20 @@ public class LoginController {
     @GetMapping("/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    /**
+     * 커스텀하게 GET 방식으로 로그아웃 처리
+     * 사실 Logout 필터의 post 방식으로 처리하는 것이 더 안전함
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if(authentication != null) {
+            // 세션 무효화 & 인증 객체 삭제, clear security context
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
