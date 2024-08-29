f4a35eaf4bd575d6955e9d804563928e7b9ee5e6
jcl <chaelynjang@lotte.net>
2024-08-29 10:56:51 +0900

    rest 인증 커스텀 로그아웃 구현

--- src/main/java/io/security/authentication/users/controller/RestApiController.java
+++ src/main/java/io/security/authentication/users/controller/RestApiController.java
@@ -1,7 +1,12 @@
 package io.security.authentication.users.controller;
 
 import io.security.authentication.domain.dto.AccountDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +28,14 @@ public class RestApiController {
     @GetMapping("/admin")
     public AccountDTO restAdmin(@AuthenticationPrincipal AccountDTO user) {
         return user;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "logout";
     }
 }
